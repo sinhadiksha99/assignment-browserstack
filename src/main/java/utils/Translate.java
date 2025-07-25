2dda74b5c76cf1ed86f775fdf94b490460805ccc
Diksha <sinhadiksha99.com>
2025-07-25 16:37:38 +0530

    added new key

--- src/main/java/utils/Translate.java
+++ src/main/java/utils/Translate.java
@@ -10,7 +10,7 @@ public class Translate {
         String jsonPayload = "{\"from\":\"es\",\"to\":\"en\",\"q\":\"" + spanishText + "\"}";
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://rapid-translate-multi-traduction.p.rapidapi.com/t"))
-                .header("x-rapidapi-key", "581931bebfmsh919b20ab6f5d569p1761b7jsn48afacf1065a") // your API key
+                .header("x-rapidapi-key", "8a858f41f9msh286ddfb2c5b2213p1b139cjsnf109b5a8f04d") // your API key
                 .header("x-rapidapi-host", "rapid-translate-multi-traduction.p.rapidapi.com")
                 .header("Content-Type", "application/json")
                 .POST(HttpRequest.BodyPublishers.ofString(jsonPayload))
